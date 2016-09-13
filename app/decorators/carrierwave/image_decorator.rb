1bae497656fa3107aa0a975934881ded875b7770
Patrick Lehmann <lehmann@squibble.me>
2016-09-13 16:33:48 +0200

    Caching

--- app/decorators/carrierwave/image_decorator.rb
+++ app/decorators/carrierwave/image_decorator.rb
@@ -3,19 +3,19 @@
 module Carrierwave::ImageDecorator
 
   def image_url(size = :two_one)
-    # Rails.cache.fetch ['Carrierwave::ImageDecorator.image_url', object, size] do
+    Rails.cache.fetch ['Carrierwave::ImageDecorator.image_url', object, size] do
       size = size.to_s
 
-      # if object.image_processing
-      #   image_placeholder(eval("#{object.image.class.name}::#{size.upcase}_DIMENSIONS").join('x'))
-      # else
-      #   begin
+      if object.image_processing
+        image_placeholder(eval("#{object.image.class.name}::#{size.upcase}_DIMENSIONS").join('x'))
+      else
+        begin
           object.image.send(size).url
-        # rescue NoMethodError
-        #   image_placeholder(eval("#{object.image.class.name}::#{size.upcase}_DIMENSIONS").join('x'), "undefined :#{size}")
-        # end
-      # end
-    # end
+        rescue NoMethodError
+          image_placeholder(eval("#{object.image.class.name}::#{size.upcase}_DIMENSIONS").join('x'), "undefined :#{size}")
+        end
+      end
+    end
   end
 
   def image_path(size = :two_one)
